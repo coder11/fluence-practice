c2d60a8cb477945e26d8801415e322a12ede6eff
Pavel Murygin <pavel.murygin@gmail.com>
2020-12-13 20:38:00 +0300

    Some pretty basic login ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.scss";
 
-function App() {
+const start = (id: string) => {
+  console.log(id);
+};
+
+const connect = (id: string) => {
+  console.log(id);
+};
+
+const App = () => {
+  const [createRoomId, setCreateRoomId] = useState("");
+  const [connectRoomId, setConnectRoomId] = useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          <ul>
-            <li>Add fluence client</li>
-            <li>Create a service</li>
-            <li>Which does something</li>
-          </ul>
-        </p>
-      </header>
+      <div className="App-content">
+        <h1>Create new room</h1>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            start(createRoomId);
+          }}
+        >
+          <div>
+            <input
+              type="text"
+              value={createRoomId}
+              onChange={(e) => setCreateRoomId(e.target.value)}
+            />
+          </div>
+          <div>
+            <input type="submit" value="Create" />
+          </div>
+        </form>
+        <br />
+        <h1>Connect to existing room</h1>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            connect(connectRoomId);
+          }}
+        >
+          <div>
+            <input
+              type="text"
+              value={connectRoomId}
+              onChange={(e) => setConnectRoomId(e.target.value)}
+            />
+          </div>
+          <div>
+            <input type="submit" value="Connect" />
+          </div>
+        </form>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
